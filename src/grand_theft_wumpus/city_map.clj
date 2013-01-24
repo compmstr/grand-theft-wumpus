a5e987044b63c972d07dd8e55a06df5f9999e9c0
Corey <corey@undimk3>
2013-01-23 23:22:26 -0500

    Map generation work

--- src/grand_theft_wumpus/city_map.clj
+++ src/grand_theft_wumpus/city_map.clj
@@ -15,6 +15,7 @@
 (defn random-loc
   []
   (rand-int config/city-locs))
+
 (defn add-worms
   [locs]
   (loop [worm-locs (rand-set config/num-worms config/city-locs)
@@ -25,29 +26,117 @@
              (update-in locs
                         [(first worm-locs) :worm]
                         (fn [_] true))))))
+
+(defn get-city-roads
+  "Returns a set of pair sets of connected locs"
+  [locs]
+  (set
+   (flatten
+    (map (fn [loc]
+           (map
+            (fn [conn]
+              (set [loc conn]))
+            (:connections (nth locs loc))))
+         (range config/city-locs)))))
+
+(defn add-cop-roadblock
+  "Adds a cop roadblock on the connection between two indexed locs"
+  [locs pair]
+  (update-in
+   (update-in locs [(first pair) :cops]
+              conj (second pair))
+   [(second pair) :cops]
+   conj (first pair)))
+
+(defn add-cops
+  [locs]
+  (let [roads (apply vector (get-city-roads locs))
+        num-roads (count roads)]
+    (reduce (fn [locs conn]
+              (if (zero? (rand-int config/cop-odds))
+                (add-cop-roadblock locs conn)
+                locs))
+            locs roads)))
+
+(defn add-road-pair
+  [locs pair]
+  (update-in
+   (update-in locs [(first pair) :connections]
+              conj (second pair))
+   [(second pair) :connections]
+   conj (first pair)))
 (defn add-roads
   [locs]
-  (let [loc-pairs (->> (take config/city-roads (repeatedly #(rand-set 2 config/city-locs)))
+  (loop [loc-pairs (->> (take config/city-roads (repeatedly #(rand-set 2 config/city-locs)))
                        (map set)
                        set
-                       (apply vector))]
-    ))
+                       (apply vector))
+         locs locs]
+    (if (empty? loc-pairs)
+      locs
+      (recur (rest loc-pairs)
+             (add-road-pair locs (first loc-pairs))))))
+
+(defn get-connected
+  "Takes in all locs, and the index to start at"
+  [locs starting]
+  (loop [connections (set (conj (:connections (nth locs starting))
+                                starting))
+         visited [starting]
+         to-visit (:connections (nth locs starting))]
+    (if (empty? to-visit)
+      connections
+      (let [new-conns (:connections (nth locs (first to-visit)))]
+        (recur (set (concat new-conns connections))
+               (conj visited (first to-visit))
+               (concat (rest to-visit)
+                       (filter #(not (contains? connections %)) new-conns)))))))
+            
+(defn find-islands
+  [locs]
+  (loop [to-check (range config/city-locs)
+         islands []]
+    (if (empty? to-check)
+      islands
+      (let [new-island (get-connected locs (first to-check))]
+        (recur
+         (remove #(contains? new-island %) to-check)
+         (conj islands new-island))))))
 
 (defn connect-islands
-  [locs])
+  "Connect any disjoined sets of locations in the city"
+  [locs]
+  (let [islands (find-islands locs)
+        first-island (apply vector (first islands))]
+    (loop [islands (rest islands)
+           locs locs]
+      (if (empty? islands)
+        locs
+        (recur
+         (rest islands)
+         (add-road-pair
+          locs
+          [(rand-nth first-island)
+           (rand-nth (apply vector (first islands)))]))))))
 
 (defn base-city-loc
+  [id]
+  {:id id
+   :connections []
+   :worm nil
+   :cops []})
+(defn base-city
   []
-  {:connections []
-   :worm nil})
+  (apply vector
+         (map base-city-loc (range config/city-locs))))
 
 (defn generate-map
   []
-  (-> (apply vector
-             (take config/city-locs (repeatedly base-city-loc)))
+  (-> (base-city)
       (add-worms)
       (add-roads)
       (connect-islands)
+      (add-cops)
       ))
 
 (def city-map
