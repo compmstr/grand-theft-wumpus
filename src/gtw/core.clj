c6df1248cefd0fd512004b60c8a82dd59e30fbd9
Corey <corey@undiwork>
2013-04-29 12:56:48 -0400

    Moved the map file from being hard coded to using a temp file

--- src/gtw/core.clj
+++ src/gtw/core.clj
@@ -4,6 +4,8 @@
             [gtw.ui :as ui]
             gviz))
 
+(defonce map-file (.getAbsolutePath (java.io.File/createTempFile "gtw.game-map" ".png")))
+
 (defonce game-map (city-map/generate-map))
 (defn game-dot-string
   []
@@ -16,8 +18,8 @@
 (declare game-move)
 (defn game-display
   [new-loc]
-  (gviz/render-dot-string (game-dot-string) "/tmp/gtw.game-map.png")
-  (ui/set-img "/tmp/gtw.game-map.png")
+  (gviz/render-dot-string (game-dot-string) map-file)
+  (ui/set-img map-file)
   (let [player-visited (set (:visited @player/player))]
     (ui/set-buttons (apply hash-map
                            (apply concat
@@ -49,17 +51,17 @@
 (player/start-player game-map)
 (ui/start-ui)
 (game-display (:loc @player/player))
-(.deleteOnExit (java.io.File. "/tmp/gtw.game-map.png"))
+(.deleteOnExit (java.io.File. map-file))
 ;;(print (game-dot-string))
 ;;(ui/start-ui)
-;;(gviz/render-dot-string (game-dot-string) "/tmp/gtw.game-map.png")
+;;(gviz/render-dot-string (game-dot-string) map-file)
 ;;Delete the map file when we're done with the game
 
-;;(ui/set-img "/tmp/gtw.game-map.png")
+;;(ui/set-img map-file)
 
 ;;(player/go-to game-map <new-loc>)
 
-;;(gviz/render-dot-string (game-dot-string) "game-map.png")
+;;(gviz/render-dot-string (game-dot-string) map-file)
 ;; then update the image on the UI
 
 ;;Example of displaying map
