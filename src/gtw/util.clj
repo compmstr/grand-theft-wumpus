d314070809d179f8a3136c0e8070b079d19a8a98
Corey <corey@undiwork>
2013-07-29 14:18:05 -0400

    Moved rand-set to use precondition

--- src/gtw/util.clj
+++ src/gtw/util.clj
@@ -2,6 +2,5 @@
 
 (defn rand-set
   [num max]
-  (if (< max num)
-    (throw (IllegalArgumentException. "Can't create a set with more items than available numbers")))
+  {:pre [(>= max num)]}
   (take num (shuffle (range max))))
