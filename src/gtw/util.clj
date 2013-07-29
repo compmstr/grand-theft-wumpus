cd4e7d87dd76db69395e758f0a8adc164eab3832
Corey <corey@undiwork>
2013-07-29 14:13:48 -0400

    Simplified rand-set

--- src/gtw/util.clj
+++ src/gtw/util.clj
@@ -4,9 +4,4 @@
   [num max]
   (if (< max num)
     (throw (IllegalArgumentException. "Can't create a set with more items than available numbers")))
-  (loop [rset (set (take num (repeatedly #(rand-int max))))]
-    (if (= (count rset) num)
-      (shuffle rset)
-      (recur (set (concat
-                   rset
-                   (take (- num (count rset)) (repeatedly #(rand-int max)))))))))
+  (take num (shuffle (range max))))
